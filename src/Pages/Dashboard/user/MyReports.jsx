8b9d013bf91042fc9bc118d49f6b87164fb8dab0
Muhammad <shalauddinahmedshipon@gmail.com>
2024-06-08 21:52:38 -0800

    my report route in dashboard is  done

--- src/Pages/Dashboard/user/MyReports.jsx
+++ src/Pages/Dashboard/user/MyReports.jsx
@@ -1,6 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { Link } from "react-router-dom";
+import SectionTitle from "../../../Shared/SectionTitle";
+import { FaEye } from "react-icons/fa6";
 
 
 const MyReports = () => {
@@ -18,9 +21,63 @@ const MyReports = () => {
   })
   console.log(myReports);
   return (
-    <div>
+    <div className="text-center">
+    <SectionTitle>My Reports </SectionTitle>
+
+    <div className="overflow-x-auto max-w-5xl px-10 mx-auto mb-10">
+     <table className="table">
+       {/* head */}
+       <thead>
+         <tr>
+           <th>SI</th>
+           <th>Survey Title</th>
+          
+           <th>Report</th>
+
+           <th>View Survey</th>
+         </tr>
+       </thead>
+       <tbody>
+         {/* row 1 */}
+
+
+
+         {
+           myReports.map((item,idx)=>(
+
+             <tr key={item._id}>
+             <td>{idx+1}</td>
+             <td>
+               
+                 <p>${item.title}</p>
+             </td>
+            
+            
+
+             <td>
+               {item.report}
+             </td>
+             <th>
+           <Link to={`/survey/details/${item.surveyId}`}><span className="flex items-center gap-2"><FaEye></FaEye>view</span></Link>
+             </th>
+           </tr>
+          
+
+
+
+
+
+           ))
+         }
+        
+        
+       </tbody>
       
-    </div>
+      
+
+     </table>
+   </div> 
+  </div>
   );
 };
 
