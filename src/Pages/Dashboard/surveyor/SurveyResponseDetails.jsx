3effee3f8bd690045d2d87cdd422b2a75007d25d
Muhammad <shalauddinahmedshipon@gmail.com>
2024-06-09 07:12:58 -0800

    Response route in surveyor dashboard done

--- src/Pages/Dashboard/surveyor/SurveyResponseDetails.jsx
+++ src/Pages/Dashboard/surveyor/SurveyResponseDetails.jsx
@@ -5,9 +5,19 @@ import SectionTitle from "../../../Shared/SectionTitle";
 import { FaVoteYea } from "react-icons/fa";
 import {  FaChartPie,  FaTable } from "react-icons/fa6";
 import { useState } from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { PieChart, Pie, Legend } from 'recharts';
 
+const colors = ['#0088FE',  'red', ];
+const COLORS = ['#0088FE',  'red',];
 
 const SurveyResponseDetails = () => {
+
+
+
+
+
+
   const [toggle,setToggle] = useState(false);
   const {id} = useParams();
   const axiosSecure = useAxiosSecure();
@@ -36,6 +46,44 @@ const SurveyResponseDetails = () => {
     setToggle(!toggle);
   }
 
+
+
+           // pi chart 
+       const RADIAN = Math.PI / 180;
+       const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+         const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+         const x = cx + radius * Math.cos(-midAngle * RADIAN);
+         const y = cy + radius * Math.sin(-midAngle * RADIAN);
+       
+         return (
+           <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+             {`${(percent * 100).toFixed(0)}%`}
+           </text>
+         );
+       };
+
+
+        //  bar chart 
+        const getPath = (x, y, width, height) => {
+          return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+          ${x + width / 2}, ${y}
+          C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+          Z`;
+        };
+
+
+        const TriangleBar = (props) => {
+          const { fill, x, y, width, height } = props;
+        
+          return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+        };
+
+  const data =[
+    {name:'yes vote', value:yesVotes},
+    {name:'no vote', value:noVotes}
+  ]
+
+
   return (
     <div>
        
@@ -53,7 +101,67 @@ const SurveyResponseDetails = () => {
 
    {
     toggle?
-     <h1 className="text-5xl">Pi chart</h1>
+    <div className="flex flex-col lg:flex-row md:justify-center md:items-center">
+          {/* pi chart  */}
+<div className="lg:w-1/2">
+
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+        <Legend></Legend>
+        </PieChart>
+    
+    
+
+
+</div>
+
+       {/* bar chart  */}
+
+<div className="w-1/2 ">
+<BarChart
+      width={350}
+      height={300}
+      data={data}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Bar dataKey="value" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+        ))}
+      </Bar>
+    </BarChart>
+
+
+</div>
+
+
+
+
+
+
+    </div>
+     
     :
     <div className="overflow-x-auto max-w-5xl px-10 mx-auto mb-10">
     <table className="table">
